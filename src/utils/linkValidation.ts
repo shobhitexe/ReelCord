2d82cc77bcb7c6eef2609b27d9dccc18d58e4c08
Sh0bhit <sguptamds94@gmail.com>
2023-06-20 17:01:57 +0530

    fix(regex): only accepts reels for now

--- src/utils/linkValidation.ts
+++ src/utils/linkValidation.ts
@@ -1,5 +1,6 @@
 export function isInstagramPostLink(link: string): boolean {
   const regex =
-    /((?:https?:\/\/)?(?:www\.)?instagram\.com\/(?:p|reel)\/([^/?#&]+)).*/g;
+    /((?:https?:\/\/)?(?:www\.)?instagram\.com\/reel\/([^/?#&]+)).*/g;
+
   return regex.test(link);
 }
