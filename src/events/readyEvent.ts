1682a07dfb57a9546382930230bd2f5d60ff1727
Sh0bhit <sguptamds94@gmail.com>
2023-06-24 19:41:28 +0530

    fix(activity): activity message changed

--- src/events/readyEvent.ts
+++ src/events/readyEvent.ts
@@ -4,7 +4,7 @@ export function handleReadyEvent(client: Client) {
   return () => {
     console.log(`Ready! Logged in as ${client.user?.tag}`);
     client.user?.setPresence({ status: "online" });
-    client.user?.setActivity("Your Words", {
+    client.user?.setActivity("Links to preview", {
       type: ActivityType.Watching,
     });
   };
