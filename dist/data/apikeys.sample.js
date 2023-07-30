27244d904d0506b5e6aa2d89513ef566a41ea494
Sh0bhit <sguptamds94@gmail.com>
2023-07-30 16:39:45 +0530

    removed dist

--- dist/data/apikeys.sample.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.apiKeys = void 0;
-exports.apiKeys = ["key1", "key2", "rest keys......"];
-//# sourceMappingURL=apikeys.sample.js.map
--- dist/utils/linkValidation.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isInstagramPostLink = void 0;
-function isInstagramPostLink(link) {
-    const regex = /((?:https?:\/\/)?(?:www\.)?instagram\.com\/(?:p|reel)\/([^/?#&]+)).*/g;
-    return regex.test(link);
-}
-exports.isInstagramPostLink = isInstagramPostLink;
-//# sourceMappingURL=linkValidation.js.map
